98cd7c792e87920573412951bcc0c5c61d56843a
Mariana-B-92 <2200640@estudante.uab.pt>
2025-05-08 15:28:13 +0100

    Update __init__.py - Atualização do layout

--- graficos/src/graficos/__init__.py
+++ graficos/src/graficos/__init__.py
@@ -1,7 +1,9 @@
 from graficos.controller import Controller
-
-
+from graficos.model import Model
+from graficos.view import View
 
 def main() -> None:
-    controller = Controller()
+    view = View()
+    model = Model(view)
+    controller = Controller(view, model)
     controller.run()
