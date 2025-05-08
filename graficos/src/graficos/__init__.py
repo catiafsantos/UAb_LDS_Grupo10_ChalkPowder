7cdc54572b67bac24157b47acd4a1bdc1c426e40
Mariana-B-92 <2200640@estudante.uab.pt>
2025-05-08 16:36:28 +0100

    Update __init__.py - Reversão para a versão anterior

--- graficos/src/graficos/__init__.py
+++ graficos/src/graficos/__init__.py
@@ -1,9 +1,6 @@
 from graficos.controller import Controller
-from graficos.model import Model
-from graficos.view import View
+
 
 def main() -> None:
-    view = View()
-    model = Model(view)
-    controller = Controller(view, model)
+    controller = Controller()
     controller.run()
